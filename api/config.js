64c90eae843f6d93b6112a6b38e50a138467bd0f
ito-72 <153911389+ito-72@users.noreply.github.com>
2025-03-26 19:37:28 +0900

    Add files via upload

--- api/config.js
+++ api/config.js
@@ -1,4 +1,4 @@
 // api/config.js
 
-export const GAS_URL = "https://script.google.com/macros/s/AKfycbwINFICAkBl5VboHGCh-whMU6YOPwLPuhtOwIKqMKJv4fz3OIZLP8GqwZNJhpkObddoJQ/exec";
+export const GAS_URL = "https://script.google.com/macros/s/AKfycbyLAqz2RqN4AbVFSaZUrrwtDM-NS6zspomqrsYUBfucwT7AvQVdw7ECPApC31pj9rkyBg/exec";
 // ↑ ← あなたの本物のURLをここに貼ってください
